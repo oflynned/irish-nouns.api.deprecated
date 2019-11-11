d7368b5b8c5f26411ff0bdc0939749710fbee5b1
Edmond O'Flynn <oflynned@tcd.ie>
2019-11-11 13:42:07 +0100

    Add data access db folder

---
+++ src/dataAccess/experimentsDb.js
@@ -0,0 +1,65 @@
+import Id from "../entities/id";
+
+const collectionName = "experiments";
+
+export default function makeExperimentsDb ({ makeDb }) {
+  return Object.freeze({
+    findAll,
+    findByExperimentId,
+    findByUserId,
+    findByCategory,
+    insert
+  });
+
+  async function findAll () {
+    const db = await makeDb();
+    const query = {};
+    const results = await db.collection(collectionName).find(query);
+    if (results.length() === 0) {
+      return [];
+    }
+
+    return (await results.toArray())
+      .map(({ _id, ...props }) => ({ id: _id, ...props }));
+  }
+
+  async function findByExperimentId ({ id: _id }) {
+    const db = await makeDb();
+    const results = await db.collection(collectionName).find({ _id }).toArray();
+    if (results.length() === 0) {
+      return [];
+    }
+
+    return (await results.toArray())
+      .map(({ _id, ...props }) => ({ id: _id, ...props }));
+  }
+
+  async function findByUserId ({ userId }) {
+    const db = await makeDb();
+    const results = await db.collection(collectionName).find({ category }).toArray();
+    if (results.length() === 0) {
+      return [];
+    }
+
+    return (await result.toArray())
+      .map(({ _id, ...props }) => ({ id: _id, ...props }));
+  }
+
+  async function findByCategory ({ category }) {
+    const db = await makeDb();
+    const results = await db.collection(collectionName).find({ category }).toArray();
+    if (results.length() === 0) {
+      return [];
+    }
+
+    return (await result.toArray())
+      .map(({ _id, ...props }) => ({ id: _id, ...props }));
+  }
+
+  async function insert ({ id: _id = Id.makeId(), ...experimentInfo }) {
+    const db = await makeDb();
+    const result = await db.collection(collectionName).insertOne({ _id, ...experimentInfo });
+    const { _id: id, ...insertedInfo } = result.ops[0];
+    return { id, ...insertedInfo };
+  }
+}
